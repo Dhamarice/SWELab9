a91ded36baf7d09944f5aac9c4029b83ded69e93
dhamarice <ruvimbomagweregwede@gmail.com>
2021-07-06 14:35:52 -0500

    finished endpoints

--- src/main/java/edu/miu/cs/cs425/eregistrar/controller/StudentController.java
+++ src/main/java/edu/miu/cs/cs425/eregistrar/controller/StudentController.java
@@ -12,11 +12,46 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping(value="/eRegistrar/api/student")
 public class StudentController {
 
-    @Autowired
-    StudentService studentService;
+    private StudentService studentService;
 
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
 
+//    http://localhost/eregistrar/api/student/list - Displays JSON array of Students data.
+//    http://localhost/eregistrar/api/student/register - Register a new Student into the system
+//    http://localhost/eregistrar/api/student/get/1 - Read/display a Student data for student with ID, 1.
+//    http://localhost/eregistrar/api/student/delete/3 - Delete the student data for student with ID, 3.
+
+    @GetMapping(value={"/list"})
+    public List<Student> listStudent(){
+        return studentService.findAllStudents();
+    }
+
+    @GetMapping(value={"/{id}"})
+    public Student student(@PathVariable Long id){
+        return studentService.findStudent(id);
+    }
+
+    @PostMapping("/register")
+    public Student RegisterStudent(@Valid @RequestBody Student student){
+     return studentService.saveStudent(student);
+
+    }
+
+    @PostMapping(value={"/update"})
+    public Student updateStudent(@RequestBody Student student){
+
+        return studentService.saveStudent(student);
+    }
+
+    @GetMapping("/delete/{id}")
+    public Student deleteStudent(@PathVariable Long id){
+        Student student = studentService.findStudent(id);
+        return studentService.deleteStudent(student);
+    }
 
 }
